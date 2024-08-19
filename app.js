c56bf3064a690bec787ee5804498e9f14deb1831
Eduardo Paes <j.eduardopaes@gmail.com>
2024-08-19 09:55:12 -0400

    Desafios 4, 5 e 6 concluidos

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 //Segundo Desafio
 document.querySelector('h1').innerHTML = 'Hora do Desafio';
 
-//Segundo Desafio 
+//Terceiro Desafio 
 // Função foi adicionada ou Botão "Console"
 function exibirMsgNoConsole(){
   console.log('O botão foi clicado.');
 }
 
+//Quarto Desafio
+// Função adicionada ao Botão "Alerta"
+function exibirMsgAlerta(){
+  alert('Eu amo JS');
+}
+
+//Quinto Desafio
+// Função adicionada ao Botão "Prompt"
+function exibirMsgPrompt(){
+  let cidade = prompt('Digite o nome de uma cidade do Brasil: ');
+  alert(`Estive em ${cidade} e lembrei de você.`);
+}
+
+//Sexto Desafio
+// Função adicionada ao Botão "Soma"
+function exibirSoma(){
+  let num1 = parseInt(prompt('Digite um número inteiro: '));
+  let num2 = parseInt(prompt('Digite outro número inteiro: '));
+
+  alert(`${num1} + ${num2} = ${num1+num2}`);
+}
