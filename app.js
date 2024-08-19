e1464f957a7da1e945a5740b1ab810436789e893
Eduardo Paes <j.eduardopaes@gmail.com>
2024-08-19 09:19:49 -0400

    Desafios 1 2 e 3 concluidos

---
+++ app.js
@@ -0,0 +1,9 @@
+//Segundo Desafio
+document.querySelector('h1').innerHTML = 'Hora do Desafio';
+
+//Segundo Desafio 
+// Função foi adicionada ou Botão "Console"
+function exibirMsgNoConsole(){
+  console.log('O botão foi clicado.');
+}
+
